1cc3f351435108d525e7d21bc70fcd2b86deb0e0
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-01 16:06:58 +0300

    add dsacco

--- src/components/Dsacco.js
+++ src/components/Dsacco.js
@@ -4,7 +4,7 @@ function Dsacco() {
   return (
 <>
 <section className='w-full m-auto px-11 text-[1.2em]'>
-  <h1 className=' font-poppins font-extrabold text-black text-center text-[2em] '>D-SACCO</h1>
+  <h1 className=' font-poppins font-extrabold text-black text-left ml-48 text-[2em] '>D-SACCO</h1>
   <p><span className='font-bold '> D-SCACCO </span> is an API solution for Micro-finance and saving co-operatives </p>
   <h2 className='w-full  pt-4 font-bold '>Designed with a User-friendly interface, D-SCACCO serves the following:-</h2>
   <ul className='ml-16 list-disc'>
@@ -19,33 +19,33 @@ function Dsacco() {
 <h3 className='ml-8 mt-2 underline text-stone-800 font-semibold'>Module Shipped with D-SCACCO</h3>
 <p className='font-bold mt-4'>1. ACCOUNTS MODULE</p>
 <ul className='ml-16 list-disc'>
-<li className='list-none'>This Module has the following sub-modules</li>
-<li>Student Registration</li>
-<li>School Fees payment and reciept issuing</li>
-<li>Record for special payments by students like Medical Fees, School Requirements</li>
-<li>Automatic academic grading  for students </li>
-<li>Semester report generation for students</li>
-<li>Captures all Student data like  information about Guardian</li>
+<li>Member can  <span className='font-bold '> open account </span> under Savings, Fixed, Business and many other categories </li>
+<li>Access to detailed information about the account any time of the day</li>
+
 </ul>
-<p className='font-bold mt-4'>2. STAFF MODULE</p>
+<p className='font-bold mt-4'>2. LOAN MODULE</p>
 <ul className='ml-16 list-disc'>
 
-<li>The staff module incoperates the following</li>
-<li>Staff Registration and Onboarding  </li>
-<li>Payroll and arrears calculation</li>
+<li>Easy <span className='font-bold '> Loan Application </span> using a well structured loan form </li>
+<li>  <span className='font-bold '> Loan Approval </span> interface for loan managers </li>
+<li>A <span className='font-bold '> Loan Repayement Form </span> with pre-populated but customized fields for loan paymement</li>
 </ul>
-<p className='font-bold mt-4'>3. LIBRARY MODULE</p>
+<p className='font-bold mt-4'>3. CASH DEPOSIT MODULE</p>
 <ul className='ml-16 list-disc'>
-<li>Digital Library where books are stored and can be accessed</li>
-<li>Tracker for Library assets usage </li>
-<li>Captures all details about a library asset</li>
+<li>Perform  <span className='font-bold '> Cash Deposit </span> on specific member account </li>
+<li> Access to <span className='font-bold '> Periodic Cash Deposits </span> performed on any member account </li>
 </ul>
-<p className='font-bold mt-4'>4. ASSETS MODULE</p>
+<p className='font-bold mt-4'>3. CASH WITHDRAW MODULE</p>
 <ul className='ml-16 list-disc'>
-<li>Asset Registration</li>
-<li>Asset tracker including :- borrowing, depreciation, value appreciation and  information about supplier</li> 
+<li>Perform  <span className='font-bold '> Cash withdraw </span> on specific member account </li>
+<li> Access to <span className='font-bold '> Periodic Cash withdraws </span> performed on any member account </li>
 </ul>
-<p className='font-bold mt-4 uppercase'>5. Income and Expenditure Tracker</p>
+
+<p className='font-bold mt-4 '>5. SHARES </p>
+<ul className='ml-16 list-disc'>
+<li>Seamless  <span className='font-bold '>Purchase  </span> of shares of a Micro-finance </li>
+<li>Ability to <span className='font-bold '> Sell </span>   shares owned by member  in a Micro-finance </li>
+</ul>
     </section></>  )
 }
 
