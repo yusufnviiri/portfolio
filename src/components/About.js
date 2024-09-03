a725407866f6d7d59ba86a996271a72c896a0c7f
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-03 12:10:10 +0300

    fix formatting

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,25 @@ import React from 'react'
 function About() {
   return (
     <>
-    
-    <div>About</div>
-    <p className='font-poppins mt-4 mb-2 text-[0.7em]  sm:text-[0.9em] sm:w-full'>
-    We strive to deliver analysis of the highest quality, leading to
-     actionable insights for our clients.  Our priority is to ensure that
-      our research is impactful and designed to answer the questions our clients have.
+    <h1 className='lg:text-[4em] text-[2em] pt-4 text-center lg:my-14 my-5'>ABOUT ME</h1>
+
+    <div className="text-[1.2em] pt-11   lg:h-[80vh] flex-col  sm:w-[90%] lg:w-[55em] xl:w-[65em] m-auto  gap-16 px-10 lg:rounded-[4em] rounded-[1em] bg-white  text-stone-900 font-poppins lg:border-[30px] border-[15px] border-emerald-900">
+    <p className='my-3'>
+    Hi, I'm [Your Name], a passionate software developer with a love for creating innovative and efficient solutions. With 4 years of experience in the industry, I specialize in developing robust web applications and APIs using modern technologies like ASP.NET Core, Docker, and JavaScript frameworks (REACT).
      </p>
+     <p className='my-3'>
+     I hold a Bachelors in Electrical Engineeering from Makerere University, where I honed my technical skills in Problem solving, ENgineering and creative thinking both front-end and back-end development. My journey into software development started with a fascination for problem-solving and a desire to build tools that make people's lives easier.
+     </p>
+     <p className='my-3'>
+     My journey began with a curiosity for how things work, which naturally led me to the world of programming. Over the years, I've had the privilege of working on diverse projects, ranging from small business websites to large-scale enterprise applications. Each project has taught me the importance of writing code that not only functions well but is also maintainable and adaptable to future needs.
+     </p>
+     <p className='my-3'>
+     Over the years, I've worked on a diverse range of projects, from building scalable e-commerce platforms to developing complex APIs that serve thousands of users daily. My work is driven by a strong commitment to quality, performance, and continuous learning.
+     </p>
+     <p className='my-3'>
+     When I'm not coding, you can find me exploring new tech trends, contributing to open-source projects, or sharing my knowledge through blog posts and talks. I'm always excited to take on new challenges and collaborate with like-minded professionals to create something amazing.
+     </p>
+     </div>
     </>
   )
 }
