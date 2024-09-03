904a2d4bd746df539f955010f64655aac4714daa
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-03 10:13:57 +0300

    add border

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,9 +6,9 @@ function Projects() {
   return (
 <>
 <h1 className='text-[4em] text-center my-14'>PROJECTS</h1>
-<div className="flex w-2/3 m-auto flex-row gap-5 rounded bg-white text-stone-900 font-poppins border-[40px] border-lime-900">
-  <img src={astronount}  alt='dsacco' className='w-1/2 rounded' />
-  <div className='w-1/2 mx-7'>
+<div className="text-[1.2em]  h-[80vh] flex flex-col lg:flex-row  sm:w-2/3 lg:w-[65em] m-auto  gap-16 px-10 rounded sm:bg-white md:bg-lime-500 lg:bg-rose-100 xl:bg-indigo-200 2xl:bg-sky-300 text-stone-900 font-poppins border-[40px] border-lime-900">
+  <img src={astronount}  alt='dsacco' className='w-1/2 h-[37em] m-auto rounded' />
+  <div className='w-1/2 '>
     <h1 className='font-bold text-center mt-10 mb-3 text-[2em]'>DSACCO</h1>
     <p className='my-3'> D-SCACCO   is an API solution for Micro-finance and saving co-operatives. The solution offers intuitive functionalities suited for a modern credit and savings association </p> 
     <p className='my-3'>Dsacco is cleverly tailored to solve all challenges encountered in Micro-Finance institutions. Designed with well tested functionalities like <span className='font-bold'>Accounts module, </span>  <span className='font-bold'>Saving module, </span><span className='font-bold'>Shares module </span><span className='font-bold'>Withdraw module  </span> and <span className='font-bold'>Deposits module  </span>    </p>
