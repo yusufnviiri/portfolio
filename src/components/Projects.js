70e4d33c262a66c5b4b841afa4ef2125c6194e1b
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-03 11:25:25 +0300

    fix formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,11 +17,11 @@ function Projects() {
     <p className='my-3'>D-sacco is cleverly tailored to solve all challenges encountered in Micro-Finance institutions. <br/> Designed with well tested functionalities like <span className='font-bold'>Accounts module, </span>  <span className='font-bold'>Saving module, </span><span className='font-bold'>Shares module </span><span className='font-bold'>Withdraw module  </span> and <span className='font-bold'>Deposits module  </span>    </p>
     
 
-    <div className='flex flex-row  lg:my-5  my-10 '>
+    <div className='flex flex-row  lg:my-5  my-10  '>
     <Link to="/" className='bg-blue-900 mr-5 capitalize  rounded px-3 py-2 text-center text-[0.9em] text-white font-bold'>Live Demo</Link>
     <Link to="/" className='bg-blue-900 mr-5 capitalize  rounded text-[0.9em] text-white font-bold px-3 py-2 text-center'>Source Code</Link> <Link to="/" className='bg-blue-900 capitalize  px-3 py-2 text-center rounded  text-[0.9em] text-white font-bold'>Video</Link>
     </div>
-    <p  className='bg-green-950 p-3 text-yellow-500  text-[0.7em] text-center'><span className='block uppercase font-bold text-yellow-300 text-[1.19em] '>languanges and frameworks </span> C#, MySQL database and Entity Framework Core </p>
+    <p  className='bg-green-950 p-3 text-yellow-500  text-[0.7em] w-2/3 text-center m-autotext-center'><span className='block uppercase font-bold text-yellow-300 text-[1.19em] '>languanges and frameworks </span> C#, MySQL and Entity Framework Core </p>
      </div>
   
 
