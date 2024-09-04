4f1c11cbf079752d11ec95e1f41a4d0c8a5e153e
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-09-04 14:05:15 +0300

    fix formatting

--- src/components/About.js
+++ src/components/About.js
@@ -11,16 +11,14 @@ function About() {
     Hi, I'm Yusuf Nviiri, a passionate software developer with a love for creating innovative and efficient solutions. With 4 years of experience in the industry, I specialize in developing robust web applications and APIs using modern technologies like ASP.NET Core, Docker, and JavaScript frameworks (REACT).
      </p>
      <p className='my-3'>
-     I hold a Bachelors in Electrical Engineeering from Makerere University, where I honed my technical skills in Problem solving, Engineering and creative thinking. <br/>
+     I hold a Bachelors Degree in Electrical Engineeering from Makerere University, where I honed my technical skills in Problem solving, Engineering and creative thinking. <br/>
      I am a certified Full-Stack software developer from Microverse, an online software training institution, where I developed skills in remote pair-programming , mastered algorithms, data structures and software development.
 
      </p>
      <p className='my-3'>
      My journey began with a curiosity for how things work, which naturally led me to the world of programming. Over the years, I've had the privilege of working on diverse projects, ranging from small business websites to large-scale enterprise applications. Each project has taught me the importance of writing code that not only functions well but is also maintainable and adaptable to future needs.
      </p>
-     <p className='my-3'>
-     Over the years, I've worked on a diverse range of projects, from building scalable e-commerce platforms to developing complex APIs . My work is driven by a strong commitment to quality, performance, and continuous learning.
-     </p>
+     
      <p className='my-3'>
      When I'm not coding, I explore new tech trends, contributing to open-source projects, or sharing my knowledge through blog posts and talks. I'm always excited to take on new challenges and collaborate with like-minded professionals to create something amazing.
      </p>
