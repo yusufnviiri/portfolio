9f0094e0656b6b4f7386e8ce7830c71061bcbac2
yusufnviiri <yusufnviiri@gmail.com>
2024-09-04 23:47:02 +0300

    add dschool

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,9 +6,9 @@ function Projects() {
   return (
 <>
 <h1 className='lg:text-[4em] text-[2em] pt-4 text-center lg:my-14 my-5'>PROJECTS</h1>
-<div className="2xl:text-[1.2em]  text-[0.89em] lg:mb-24 mb-10    flex flex-col lg:flex-row  sm:w-[90%] lg:w-[55em] 2xl:w-[65em] m-auto  gap-16 px-10  rounded-[1em] bg-white  text-stone-900 font-poppins lg:border-[2px] border-[15px] border-emerald-900">
-  <div className='lg:w-1/2 lg:flex flex-col justify-around lg:pl-16'>
-  <h1 className='font-bold text-left my-10  lg:text-[3em] text-[2em]'><span className='underline underline-offset-[0.5em]'>D-SAC</span>CO</h1>
+<div className="2xl:text-[1.2em]  text-[0.89em] lg:mb-24 mb-10    flex flex-col lg:flex-row  sm:w-[90%] lg:w-[55em] 2xl:w-[65em] m-auto  gap-11 px-10  rounded-[1em] bg-white  text-stone-900 font-poppins lg:border-[2px] border-[15px] border-emerald-900">
+  <div className='lg:w-1/2 lg:flex flex-col justify-around '>
+  <h1 className='lg:pl-10 font-bold text-left my-10  lg:text-[3em] text-[2em]'><span className='underline underline-offset-[0.5em]'>D-SAC</span>CO</h1>
   <img src={astronount}  alt='dsacco' className='m-auto w-[90%] lg:h-[60%] h-[30em]  rounded' />
   </div>
 
@@ -31,9 +31,9 @@ function Projects() {
 
   </div>
 
-  <div className="2xl:text-[1.2em] text-[0.89em] lg:mb-24 mb-10    flex flex-col lg:flex-row  sm:w-[90%] lg:w-[55em] 2xl:w-[65em] m-auto  gap-16 px-10  rounded-[1em] bg-white  text-stone-900 font-poppins lg:border-[2px] border-[15px] border-emerald-900">
-  <div className='lg:w-1/2 lg:flex flex-col justify-around lg:pl-16'>
-  <h1 className='font-bold text-left my-10  lg:text-[3em] text-[2em]'><span className='underline underline-offset-[0.5em]'>D-SCHO</span>OL</h1>
+  <div className="2xl:text-[1.2em] text-[0.89em] lg:mb-24 mb-10    flex flex-col lg:flex-row  sm:w-[90%] lg:w-[55em] 2xl:w-[65em] m-auto  gap-11 px-10  rounded-[1em] bg-white  text-stone-900 font-poppins lg:border-[2px] border-[15px] border-emerald-900">
+  <div className='lg:w-1/2 lg:flex flex-col justify-around '>
+  <h1 className=' lg:pl-10 font-bold text-left my-10  lg:text-[3em] text-[2em]'><span className='underline underline-offset-[0.5em]'>D-SCHO</span>OL</h1>
   <img src={astronount}  alt='dsacco' className='m-auto w-[90%] lg:h-[60%] h-[30em]  rounded' />
   </div>
 
