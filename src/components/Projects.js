acadd30fe85fe89cff6f712aa697cc23e976d407
yusufnviiri <yusufnviiri@gmail.com>
2024-09-06 18:22:28 +0300

    add video links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,9 +22,8 @@ function Projects() {
 
     <div className='flex flex-row justify-between 2xl:w-[60%] w-[70%] lg:w-full    my-5 lg:text-[0.7em] 2xl:text-[0.98em]   '>
     <Link to="/dsacco" className='bg-blue-900  capitalize  rounded px-3 py-2 text-center  text-white font-bold'>Details</Link>
-    <Link to="https://github.com/yusufnviiri/DsaccoAPP.git" className='bg-blue-900  capitalize  rounded  text-white font-bold px-3 py-2 text-center'> Code Back-End</Link> <Link to="https://www.loom.com/share/6aeb704e275a4ba38f6db027fc048a54?sid=99bd13f7-99ef-4806-9197-97735ba412fc" className='bg-blue-900 capitalize  px-3 py-2 text-center rounded    text-white font-bold'>Video</Link>
-    <Link to="https://www.loom.com/share/ebe2b6107a2043e79ed8a027f1948f33?sid=73de4ac2-3bbb-42e0-a8eb-c0c51b8b8dc8" className='bg-blue-900  capitalize  rounded px-3 py-2 text-center w-fit text-white font-bold'>Video 1</Link>
-    <Link to="https://www.loom.com/share/9370419c3ef141a2b6836c34990fbb87?sid=f7eba16d-e416-48b1-a63e-6fadc8c5586f" className='bg-blue-900  capitalize  rounded px-3 py-2 text-center w-fit text-white font-bold'>Video 2</Link>
+    <Link to="https://github.com/yusufnviiri/dsaccofront.git" className='bg-blue-900  capitalize  rounded  text-white font-bold px-3 py-2 text-center'> Code Front-End</Link> 
+    <Link to="https://github.com/yusufnviiri/DsaccoAPP.git" className='bg-blue-900  capitalize  rounded  text-white font-bold px-3 py-2 text-center'> Code Back-End</Link> <Link to="https://www.loom.com/share/6aeb704e275a4ba38f6db027fc048a54?sid=99bd13f7-99ef-4806-9197-97735ba412fc" className='bg-blue-900 capitalize  px-3 py-2 text-center rounded    text-white font-bold'>Video</Link>   
     </div>
     <p  className='bg-green-950 p-3 mt-4 text-yellow-500  text-[0.7em] w-2/3 sm:w-full xl:w-2/3  m-auto text-center'><span className='block uppercase font-bold text-yellow-300 text-[1.19em] '>Languanges and Frameworks </span> C#, MySQL and Entity Framework Core and React </p>
      </div>
@@ -46,9 +45,15 @@ function Projects() {
     <p className='my-3'>D-sacco is cleverly tailored to solve all challenges encountered in Micro-Finance institutions. <br/> Designed with well tested functionalities like <span className='font-bold'>Accounts module, </span>  <span className='font-bold'>Saving module, </span><span className='font-bold'>Shares module </span><span className='font-bold'>Withdraw module  </span> and <span className='font-bold'>Deposits module  </span>    </p>
     
 
-    <div className='flex flex-row    my-5 lg:text-[0.7em] 2xl:text-[0.98em]   '>    <Link to="/dsacco" className='bg-blue-900 mr-5 capitalize  rounded px-3 py-2 text-center  text-white font-bold'>Details</Link>
+    <div className='flex flex-row    my-5 lg:text-[0.7em] 2xl:text-[0.98em]'>    <Link to="/dschool" className='bg-blue-900 mr-5 capitalize  rounded px-3 py-2 text-center  text-white font-bold'>Details</Link>
     <Link to="https://github.com/yusufnviiri/dschool.git" className='bg-blue-900 mr-5 capitalize  rounded  text-white font-bold px-3 py-2 text-center'> Code</Link>
-     <Link to="https://www.loom.com/share/6aeb704e275a4ba38f6db027fc048a54?sid=99bd13f7-99ef-4806-9197-97735ba412fc" className='bg-blue-900 capitalize  px-3 py-2 text-center rounded  mr-5 text-white font-bold'>Video</Link>
+     
+     <Link to="https://www.loom.com/share/9370419c3ef141a2b6836c34990fbb87?sid=f7eba16d-e416-48b1-a63e-6fadc8c5586f" className='bg-blue-900 mr-5 capitalize  rounded  text-white font-bold px-3 py-2 text-center'> Video 1</Link>
+
+     <Link to="https://www.loom.com/share/ebe2b6107a2043e79ed8a027f1948f33?sid=73de4ac2-3bbb-42e0-a8eb-c0c51b8b8dc8" className='bg-blue-900 mr-5 capitalize  rounded  text-white font-bold px-3 py-2 text-center'> Video 2</Link>
+
+
+     
     
     </div>
     <p  className='bg-green-950 p-3 mt-4 text-yellow-500  text-[0.7em] w-2/3 sm:w-full xl:w-2/3  m-auto text-center'><span className='block uppercase font-bold text-yellow-300 text-[1.19em] '>Languanges and Frameworks </span> C#, MySQL and Entity Framework Core and React </p>
